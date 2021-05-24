7e1702a3212bb101c0d8be9f5f1eb5cdf80882df
Abi Permana <andreasabipermana@gmail.com>
2021-05-24 18:32:49 +0700

    Initial Commit Nusademy Backend API

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# nusademy-backend-api
+# Strapi application
+
+A quick description of your strapi application
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '2ff28053-3882-475c-8067-c50923c1c490'
+};
