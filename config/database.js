2de7b74b900c78a6d1d2bcab7fe35ca0ce6555ef
muhnandajr <muhnandajr@gmail.com>
2021-05-30 12:18:35 +0700

    fix assign to role

--- config/database.js
+++ config/database.js
@@ -15,3 +15,23 @@ module.exports = ({ env }) => ({
   },
 });
 
+// module.exports = ({ env }) => ({
+//   defaultConnection: 'default',
+//   connections: {
+//     default: {
+//       connector: 'bookshelf',
+//       settings: {
+//         client: 'postgres',
+//         host: env('DATABASE_HOST', 'localhost'),
+//         port: env.int('DATABASE_PORT', 5432),
+//         database: env('DATABASE_NAME', 'strapi'),
+//         username: env('DATABASE_USERNAME', 'ayunkihikmawan'),
+//         password: env('DATABASE_PASSWORD', ''),
+//         schema: env('DATABASE_SCHEMA', 'public'), 
+//       },
+//       options: {
+//         ssl: env.bool('DATABASE_SSL', false),
+//       },
+//     },
+//   },
+// });
